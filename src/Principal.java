567d4eabafe13485509bc8b9b209ef1707410e30
yassern1 <yassern1@gmail.com>
2016-12-17 15:45:05 +0000

    deuxieme méthode "firstExample"

--- src/Principal.java
+++ src/Principal.java
@@ -2,6 +2,8 @@
 public class Principal {
 	public Principal(){
 		printMessage();
+		firstExample();
+		
 	}
 
 	public static void main(String[] args) {
@@ -11,5 +13,10 @@ public class Principal {
 	public void printMessage(){
 		System.out.println("Hello world!!!!!");
 	}
-
+	
+	public void firstExample(){
+		Compagnie c1= new Compagnie("id1", "RAM");
+		Compagnie c2= new Compagnie("id2", "RyanAir");
+		System.out.println(c1+"\n"+c2);
+	}
 }
